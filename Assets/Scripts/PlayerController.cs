c132469f9a6beff183a0e88a48fc3a6b37c99ba3
Joseph Ni <joseph.ni@blake.com.au>
2016-05-22 22:19:01 +1000

    added player controller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Boundary {
+	public float xMin, xMax, zMin, zMax;	
+}
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	public float speed;
+	public Boundary boundary;
+	public float tilt;
+	
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+		
+		
+		GetComponent<Rigidbody>().velocity = new Vector3 (moveHorizontal, 0.0f, moveVertical) * speed;
+		
+		GetComponent<Rigidbody>().position = new Vector3(
+			Mathf.Clamp(GetComponent<Rigidbody>().position.x, boundary.xMin, boundary.xMax), 
+			1.0f, 
+			Mathf.Clamp(GetComponent<Rigidbody>().position.z, boundary.zMin, boundary.zMax)
+			); 
+
+		GetComponent<Rigidbody>().rotation = Quaternion.Euler (0.0f, 0.0f, GetComponent<Rigidbody>().velocity.x * -tilt);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
