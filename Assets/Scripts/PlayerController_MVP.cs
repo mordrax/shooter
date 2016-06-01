059ea90ae2c48b6bce7dda4195fcf386702e111e
ChiefTripodBear <avalacos@gmail.com>
2016-06-01 21:45:52 +1000

    Fixed Player movement
    **Camera method swaps y and z plane compared to standard transform
    controls.
    
    Thus calculate transform.position.y of camera - player and input it as
    the z distance in the clamp...

--- Assets/Scripts/PlayerController_MVP.cs
+++ Assets/Scripts/PlayerController_MVP.cs
@@ -30,46 +30,39 @@ public class PlayerController_MVP : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        P1_Move();
+        CalculateBoundary();
+    }
+
+    void P1_Move()
+    {
         float moveHorizontal = Input.GetAxis("P1_Horizontal");
         float moveVertical = Input.GetAxis("P1_Vertical");
 
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
         GetComponent<Rigidbody>().velocity = movement * speed;
 
-        var boundary = new Boundary();
+        GetComponent<Rigidbody>().rotation = Quaternion.Euler(0.0f, 0.0f, GetComponent<Rigidbody>().velocity.x * -tilt);
+    }
+
+    void CalculateBoundary()
+    {
+        Vector3 player1 = GameObject.FindGameObjectWithTag("Player").transform.position;
+        float distance = Camera.main.transform.position.y - player1.y;
+
+        Vector3 origin = Camera.main.ViewportToWorldPoint(new Vector3(0.05f, 0.05f, distance));
+        Vector3 extent = Camera.main.ViewportToWorldPoint(new Vector3(0.95f, 0.6f, distance));
+        float xMin = origin.x;
+        float xMax = extent.x;
+        float zMin = origin.z;
+        float zMax = extent.z;
 
         GetComponent<Rigidbody>().position = new Vector3
         (
-            Mathf.Clamp(GetComponent<Rigidbody>().position.x, boundary.xMin, boundary.xMax),
+        Mathf.Clamp(GetComponent<Rigidbody>().position.x, xMin, xMax),
             0.0f,
-            Mathf.Clamp(GetComponent<Rigidbody>().position.z, boundary.zMin, boundary.zMax)
+            Mathf.Clamp(GetComponent<Rigidbody>().position.z, zMin, zMax)
         );
-
-        GetComponent<Rigidbody>().rotation = Quaternion.Euler(0.0f, 0.0f, GetComponent<Rigidbody>().velocity.x * -tilt);
     }
 }
-
-    public class Boundary
-    {
-        public float xMin, xMax, zMin, zMax;
-        Vector3 origin = Camera.main.ViewportToWorldPoint(new Vector3(0.05f, 0.05f, 0));
-        Vector3 extent = Camera.main.ViewportToWorldPoint(new Vector3(0.95f, 0.6f, 0));
-
-    /* - camera clamp from unity forums - try to incorporate?
-         void Update() {
-         Vector3 pos = Camera.main.WorldToViewportPoint (transform.position);
-         pos.x = Mathf.Clamp01(pos.x);
-         pos.y = Mathf.Clamp01(pos.y);
-         transform.position = Camera.main.ViewportToWorldPoint(pos);
-     }
-    */
-    void CalculateBoundary()
-        {
-        xMin = origin.x;
-        xMax = extent.x;
-        zMin = origin.z;
-        zMax = extent.z;
-        }
-               
-    }
  
