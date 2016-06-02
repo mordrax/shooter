797e189125b06fcbc06325988c34ef01757af909
kfed <kaan.duran@gmail.com>
2016-06-02 22:07:30 +1000

    Revert "Revert "Shredder box, lighting, bounding box""
    This reverts commit f4288d153ea120b58e7b6a6c6ff726b3fb94a381.

---
+++ Assets/Scripts/BulletControl.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletControl : MonoBehaviour {
+
+    public float bulletSpeed;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        GetComponent<Rigidbody>().velocity = transform.forward * bulletSpeed;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+       // GetComponent<Rigidbody>().velocity = transform.forward * bulletSpeed;
+    }
+}
